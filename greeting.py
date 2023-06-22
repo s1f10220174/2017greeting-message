f074099c2d40673428d9f320a44d7dbb128e2d9e
s1f10220174 <s1f102201740@iniad.org>
2023-06-22 14:54:13 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,6 +1,18 @@
+from datetime import datetime
 def greet(name):
     message = 'Hello, ' + name + '-san!'
     print(message)
 
+
+def greet():
+    message = 'Hello'
+    hour = datetime.now().hour
+    if hour <= 11:
+        message = 'Good morning'
+    elif hour <= 17:
+        message = 'Hello'
+    else:
+        message = 'Good evening'
+    print(message)
 
 greet('Inoue')
